ac04687821989523da585166c11c8f9b1af04ecf
seheesf88 <seheesf88@gmail.com>
2019-07-04 08:23:22 -0700

    save

--- src/Contacts/index.js
+++ src/Contacts/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './Contact.css';
 import Nav from '../Nav'
-// import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 
 class Contact extends Component{
@@ -18,8 +18,8 @@ class Contact extends Component{
         <Nav />
         <h1>Contact</h1>
         <div>Email: seheesf88@gmail.com</div>
-        <div>Linkedin: </div>
-        <div>Github: </div>
+        <div>Linkedin: <Link to=""></Link> </div>
+        <div>Github: <Link to=""></Link> </div>
         <div>Resume: <span>Download</span></div>
       </div>
     )
