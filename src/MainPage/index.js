18088cda15331f00ba83fc4c6f09f153096a1b9a
seheesf88 <seheesf88@gmail.com>
2019-07-07 23:58:30 -0700

    sth

--- src/MainPage/index.js
+++ src/MainPage/index.js
@@ -17,58 +17,77 @@ class MainPage extends Component{
   }
 
   render(){
+
+    var TxtRotate = function(el, toRotate, period) {
+      this.toRotate = toRotate;
+      this.el = el;
+      this.loopNum = 0;
+      this.period = parseInt(period, 10) || 2000;
+      this.txt = '';
+      this.tick();
+      this.isDeleting = false;
+    };
+
+    TxtRotate.prototype.tick = function() {
+      var i = this.loopNum % this.toRotate.length;
+      var fullTxt = this.toRotate[i];
+
+      if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+      } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+      }
+
+      this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+      var that = this;
+      var delta = 300 - Math.random() * 100;
+
+      if (this.isDeleting) { delta /= 2; }
+
+      if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+      } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+      }
+
+      setTimeout(function() {
+        that.tick();
+      }, delta);
+    };
+
+    window.onload = function() {
+      var elements = document.getElementsByClassName('txt-rotate');
+      for (var i=0; i<elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-rotate');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+          new TxtRotate(elements[i], JSON.parse(toRotate), period);
+        }
+      }
+      // INJECT CSS
+      var css = document.createElement("style");
+      css.type = "text/css";
+      css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+      document.body.appendChild(css);
+    };
+
+
+
     return(
     <div className="main">
       <div className="intro">
-          <p className="intro-item">Hi, I am SeHee</p>
-          <p className="intro-item">I am Web Developer</p>
-          <div className="keywords">
-            <span>#Web development </span>
-            <span>#Passion </span>
-            <span>#Javascript </span>
-            <span>#Python </span>
-            <span>#Debugging </span>
-            <span>#Customer Service </span>
-            <span>#Hackathon </span>
-            <span>#React </span>
-          </div>
-      </div>
-
-      <div className="part-one">
-        <div className="wow slideInRight"
-        data-wow-duration="2s"
-        data-wow-iteration="1"
-        data-wow-offset="10"
-        >
-          <p className="part-one-item">I am an engineer</p>
-          <p className="part-one-item">dfnkdnfkndknfk</p>
+        <div>Hi, </div>
+        <div className="title">I am
+        <span> </span>
+        <span
+         class="txt-rotate"
+         data-period="2000"
+         data-rotate='[ "SeHee", "Web Developer.", "React Lover.", "Artist.", "Creator.", "your partner!" ]'></span>
         </div>
-      </div>
-      <div className="part-two">
-
-        <div className="wow slideInLeft"
-        data-wow-duration="2s"
-        data-wow-iteration="1"
-        data-wow-offset="10"
-        >
-          <p className="part-two-item">I am an artist</p>
-          <p className="part-two-item">experienced ux/ui designer... blah</p>
-          <Link to="/projects">Check out my artstic</Link>
-        </div>
-
-
-      </div>
-      <div className="part-three">
-
-        <div className="wow slideInLeft"
-        data-wow-duration="2s"
-        data-wow-iteration="1"
-        data-wow-offset="10"
-        >
-          <p className="part-three-item">I am a connector</p>
-          <p className="part-three-item">bljkdjfkdjk</p>
-        </div>
-
       </div>
     </div>
     )
