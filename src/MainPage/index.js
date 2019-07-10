82f15603e5b05e237dd7fb8e58f82e9a566e4750
seheesf88 <seheesf88@gmail.com>
2019-07-09 20:55:30 -0700

    done

--- src/MainPage/index.js
+++ src/MainPage/index.js
@@ -89,6 +89,52 @@ class MainPage extends Component{
          data-rotate='[ "SeHee", "Web Developer.", "React Lover.", "Artist.", "Creator.", "your partner!" ]'></span>
         </div>
       </div>
+      <div className="container-summary">
+        <div className="">
+          <p className="summary one">
+            "Highly <span className="bold">analytical</span>, <span className="bold">creative</span> and <span className="bold">enthusiastic</span> web developer with a powerful blend of exceptional insights and high-level performance in building
+           and optimizing the performance of user-centric, high-impact websites for start-ups and established companies."
+          </p>
+          <div className="container-icon">
+            <div className="icon"></div>
+            <div className="icon"></div>
+            <div className="icon"></div>
+          </div>
+        </div>
+        <div>
+          <p className="summary two">
+            "An articulate developer with verifiable strengths to leverage technical, analytical and problem-solving skills to create dynamic, high-speed websites, apps and platforms fueling competitive
+           advantage and revenue growth."
+          </p>
+          <div className="container-icon">
+            <div className="icon"></div>
+            <div className="icon"></div>
+            <div className="icon"></div>
+          </div>
+        </div>
+        <div>
+          <p className="summary three">
+          "Well-equipped individual possessing an acute awareness of practical issues and trends, particularly accessibility, usability
+          and emerging technologies."
+          </p>
+          <div className="container-icon">
+            <div className="icon"></div>
+            <div className="icon"></div>
+            <div className="icon"></div>
+          </div>
+        </div>
+        <div>
+          <p className="summary four">
+          "Able to provide rapid, robust and client-acclaimed front- and back-end web development optimizing user experience, sales, search engine ranking,
+          brand positioning and related metrics, whilst demonstrating excellent communication skills."
+          </p>
+          <div className="container-icon">
+            <div className="icon"></div>
+            <div className="icon"></div>
+            <div className="icon"></div>
+          </div>
+        </div>
+      </div>
     </div>
     )
   }
