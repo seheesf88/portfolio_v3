1e5490aa8ac319f6f28399ca70090539d2bdf587
SeHee Son <seheeson@MacBook-Pro.local>
2020-09-01 00:39:09 -0700

    install git dep

--- src/AboutMe/index.js
+++ src/AboutMe/index.js
@@ -16,7 +16,7 @@ class AboutMe extends Component{
     return(
       <div>
         <Nav />
-                  <h1 className="">About Me.</h1>
+        <h1 className="">About Me.</h1>
         <div className="aboutme">
           <p className="aboutme-item">
             Highly analytical, creative and enthusiastic web developer with a powerful blend of exceptional insights and high-level performance in building and optimizing the performance of user-centric, high-impact websites for start-ups and established companies. An articulate developer with verifiable strengths to leverage technical, analytical and problem-solving skills to create dynamic, high-speed websites, apps and platforms fueling competitive advantage and revenue growth. Well-equipped individual possessing an acute awareness of practical issues and trends, particularly accessibility, usability and emerging technologies. Able to provide rapid, robust and client-acclaimed front- and back-end web development optimizing user experience, sales, search engine ranking, brand positioning and related metrics, whilst demonstrating excellent communication skills.
--- src/MainPage/index.js
+++ src/MainPage/index.js
@@ -88,7 +88,7 @@ class MainPage extends Component{
             <span
              className="txt-rotate"
              data-period="2000"
-             data-rotate='[ "SeHee", "a Web Developer.", "React Lover.", "your partner!" ]'></span>
+             data-rotate='[ "SeHee.", "a Software Engineer.", "a React Developer.", "a Problem solver.", "a team player." ]'></span>
           </div>
         </div>
 
